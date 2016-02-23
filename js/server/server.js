947e3ba4b3592aa8dfa519f1abada8cd9812b721
Ramona Völkel <ramona.voelkel@stud.uni-regensburg.de>
2016-02-23 17:17:14 +0100

    Revert "added simple server-client conncection"
    This reverts commit a0764345b1d0fa2dfb853e2a87c08413591d41e2.

--- js/server/server.js
+++ js/server/server.js
@@ -3,13 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile('main.html',{ root:'D:/OneDrive/SkripteWS2015/MME/Projekt/dunegon-hop'});
+    res.sendFile(__dirname + '/main.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
         console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
