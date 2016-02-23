a0764345b1d0fa2dfb853e2a87c08413591d41e2
Ramona Völkel <ramona.voelkel@stud.uni-regensburg.de>
2016-02-23 17:16:25 +0100

    added simple server-client conncection

--- js/server/server.js
+++ js/server/server.js
@@ -3,20 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/main.html');
-});
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+    res.sendFile('main.html',{ root:'D:/OneDrive/SkripteWS2015/MME/Projekt/dunegon-hop'});
 });
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
         io.emit('chat message', msg);
+        console.log('message: ' + msg);
     });
 });
 
