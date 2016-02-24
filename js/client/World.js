63dcf7b8c0d9657b9d6bf66c13a9be67eb060b1a
Ramona Völkel <ramona.voelkel@stud.uni-regensburg.de>
2016-02-24 17:28:36 +0100

    added countdown again

--- js/client/World.js
+++ js/client/World.js
@@ -4,6 +4,11 @@
 var DungeonHop = DungeonHop || {};
 DungeonHop.World = function () {
     var that = {};
+    var count = 3;
+    var timer;
+    var time = document.querySelector('#timer');
+    var seconds = 0;
+    var startButton = document.querySelector('#start');
     var worldMatrix = [];
     var actualChunk_Z_Position = 0;
     var models;
@@ -18,8 +23,38 @@ DungeonHop.World = function () {
         for(var z = 0; z > -16; z--){
             createChunk();
         }
+        startButton.addEventListener("click", onStartButtonClicked);
+    }
+    function onStartButtonClicked(){
+        timer = setInterval(function() { handleCountdown(count); }, 1000);
+        timer();
     }
 
+    //handles a countdown and starts the timer
+    function handleCountdown() {
+        if(count === 0) {
+            time.innerHTML = "START";
+            clearInterval(timer);
+            setInterval(startTimer, 1000);
+        } else {
+            time.innerHTML = count;
+            count--;
+        }
+    }
+
+    function startTimer() {
+        var min, sec;
+        seconds++;
+        min = Math.floor(seconds / 60);
+        sec = Math.floor(seconds % 60);
+        if (min < 10) {
+            min = "0" + min;
+        }
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+        time.innerHTML = min + ":" + sec;
+    }
     function createChunk(){
         var chunk = new DungeonHop.Chunk();
         chunk.init(scene,actualChunk_Z_Position,models);
