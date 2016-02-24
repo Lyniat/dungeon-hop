70defd83561693afb9d46e22309e3efae3346452
Ramona Völkel <ramona.voelkel@stud.uni-regensburg.de>
2016-02-24 12:34:55 +0100

    added simple chat box to test client-server connection

--- js/server/server.js
+++ js/server/server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/main.html');
+    res.sendFile('main.html', {root: 'D:/OneDrive/SkripteWS2015/MME/Projekt/dunegon-hop'});
 });
 
 io.on('connection', function(socket){
